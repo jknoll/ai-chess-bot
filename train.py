21e87a34b2b43f210569abc1ebb3b8b6e140f061
Justin F. Knoll <justin.knoll@gmail.com>
2024-07-03 14:30:58 -0700

    modified training script"

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
-!pip install peewee pytorch-lightning
-!wget https://storage.googleapis.com/chesspic/datasets/2021-07-31-lichess-evaluations-37MM.db.gz
-!gzip -d "2021-07-31-lichess-evaluations-37MM.db.gz"
-!rm "2021-07-31-lichess-evaluations-37MM.db.gz"
+#!pip install peewee pytorch-lightning
+#!wget https://storage.googleapis.com/chesspic/datasets/2021-07-31-lichess-evaluations-37MM.db.gz
+#!gzip -d "2021-07-31-lichess-evaluations-37MM.db.gz"
+#!rm "2021-07-31-lichess-evaluations-37MM.db.gz"
 
 ####
 
