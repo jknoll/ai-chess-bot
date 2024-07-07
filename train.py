cc8f188238000335831caf2c119c117e27e381ef
Justin F. Knoll <justin.knoll@gmail.com>
2024-07-07 16:16:47 -0700

    Training config updated to train for three epochs. Slightly reduced loss, but possibly at the rist of overfitting. Need to implement train/validate/test split

--- train.py
+++ train.py
@@ -87,13 +87,14 @@ class EvaluationModel(pl.LightningModule):
 
 # Originally 4, 512, 1e-3
 configs = [
-           {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-2, "max_epochs": 1},
+           {"layer_count": 4, "batch_size": 512, "learning_rate": 1e-3, "max_epochs": 3}
+"""
            {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-3, "max_epochs": 1},
            {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-4, "max_epochs": 1},      
            {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-2, "max_epochs": 1},
            {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-3, "max_epochs": 1},
            {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-4, "max_epochs": 1},
-          #  {"layer_count": 6, "batch_size": 1024},
+"""
            ]
 
 print(pl.__version__)
