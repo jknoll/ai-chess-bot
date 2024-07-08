d7aa4199ac85f35881246f6394515af4bd14e381
Justin F. Knoll <justin.knoll@gmail.com>
2024-07-08 13:50:28 -0700

    Checkpoint with TensorBoard model visualization working.

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from config import LABEL_COUNT
-from models import Evaluations
+from database_models import Evaluations
 
 print(LABEL_COUNT)
 eval = Evaluations.get(Evaluations.id == 1)
@@ -10,11 +10,12 @@ print(eval.eval)
 ####
 
 import torch
-import numpy as np
 from torch import nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 import pytorch_lightning as pl
-from pytorchdatasets import EvalationDataSet, dataset
+from pytorch-datasets import EvaluationDataset, dataset
 
 import time
 from collections import OrderedDict
@@ -40,7 +41,7 @@ class EvaluationModel(pl.LightningModule):
     loss = F.l1_loss(y_hat, y)
     self.log("train_loss", loss)
     return loss
-
+  
   def configure_optimizers(self):
     return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
@@ -48,24 +49,34 @@ class EvaluationModel(pl.LightningModule):
     dataset = EvaluationDataset(count=LABEL_COUNT)
     return DataLoader(dataset, batch_size=self.batch_size, num_workers=2, pin_memory=True)
 
+  def on_train_start(self):
+    device = self.device
+    dummy_input = torch.zeros((1, 808), device=device)
+    self.logger.experiment.add_graph(self, dummy_input)
+    super().on_train_start()
+
 # Originally 4, 512, 1e-3
 configs = [
-           {"layer_count": 4, "batch_size": 512, "learning_rate": 1e-3, "max_epochs": 3}
-"""
-           {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-3, "max_epochs": 1},
-           {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-4, "max_epochs": 1},      
-           {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-2, "max_epochs": 1},
-           {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-3, "max_epochs": 1},
-           {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-4, "max_epochs": 1},
-"""
+           {"layer_count": 4, "batch_size": 512, "learning_rate": 1e-3, "max_epochs": 1},
+
+#          {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-3, "max_epochs": 1},
+#          {"layer_count": 4, "batch_size": 256, "learning_rate": 1e-4, "max_epochs": 1},      
+#          {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-2, "max_epochs": 1},
+#          {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-3, "max_epochs": 1},
+#          {"layer_count": 6, "batch_size": 256, "learning_rate": 1e-4, "max_epochs": 1},
+
            ]
+
+# Determine the accelerator
+accelerator = 'gpu' if torch.cuda.is_available() else 'cpu'
 
 print(pl.__version__)
 for config in configs:
   version_name = f'{int(time.time())}-batch_size-{config["batch_size"]}-layer_count-{config["layer_count"]}-learning_rate-{config["learning_rate"]}'
   logger = pl.loggers.TensorBoardLogger("lightning_logs", name="chessml", version=version_name)
-  trainer = pl.Trainer(num_nodes=1,precision=16,max_epochs=config["max_epochs"],logger=logger)
+  trainer = pl.Trainer(num_nodes=1,precision=16,max_epochs=config["max_epochs"],logger=logger, accelerator=accelerator, devices=1 if torch.cuda.is_available() else 1)
   model = EvaluationModel(layer_count=config["layer_count"],batch_size=config["batch_size"],learning_rate=config["learning_rate"])
+
   # block commented out previously; appears to be for adaptive learning rate behavior, but the API has changed.
   #trainer.tune(model)
   #lr_finder = trainer.tuner.lr_find(model, min_lr=1e-6, max_lr=1e-3, num_training=25)
